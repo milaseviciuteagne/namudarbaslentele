f5128c3d89b7f59bd98ac42a2a7a1c8f8cc9120a
milaseviciuteagne <milaseviciuteagne@gmail.com>
2020-10-28 11:05:51 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+var prideti = document.getElementById('naujas');
+var duomenys = document.getElementById('duomenys');
+var trintimodala = document.getElementById('trinti_modalas');
+var modalas = document.getElementById('modalas');
+var tbody = document.querySelector('tbody');
+var deleteRow = document.querySelector('deleteRow');
+
+var mainmygtukas = document.getElementById('modalaspagrindinis_mygtukas');
+
+// modale
+var data = document.getElementById('data');
+var numeris = document.getElementById('numeris');
+var laikas = document.getElementById('laikas');
+var kelias = document.getElementById('kelias');
+var redaguotiDuomenis = false;
+const duomenys = [];
